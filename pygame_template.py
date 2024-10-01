d7986b891eba44a08440a8e40e85371da2548246
nec2dev <nec2multivackergithub@outlook.com>
2024-10-01 11:43:30 -0300

    Encuentro 027

--- pygame_template.py
+++ pygame_template.py
@@ -5,11 +5,14 @@ pygame.init()
 # Crear la ventana y poner el tamaño.
 screen = pygame.display.set_mode((640, 360))
 # Poner el título de la ventana.
-pygame.display.set_caption("Mi Juego")
+pygame.display.set_caption("PES 2024")
+# definir un icono y agregarlo a la ventana
+icon = pygame.image.load("img/videogame.png")
+pygame.display.set_icon(icon)
 # Crear la superficie del fondo o background.
 imgBackground = pygame.Surface(screen.get_size())
 imgBackground = imgBackground.convert()
-imgBackground.fill((0, 0, 255))
+imgBackground.fill((0, 255, 0))
 # Inicializar las variables de control del game loop.
 clock = pygame.time.Clock()
 salir = False
@@ -19,9 +22,8 @@ while not salir:
     clock.tick(60)
     # Procesar los eventos que llegan a la aplicación.
     for event in pygame.event.get():
-    # Si se cierra la ventana se sale del programa.
+        # Si se cierra la ventana se sale del programa.
         if event.type == pygame.QUIT:
-            salir = True
             # Si se pulsa la tecla [Esc] se sale del programa.
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_ESCAPE:
